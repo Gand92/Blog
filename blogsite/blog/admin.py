55bb2d7f11f61a5d6ef879f26750a1f8255023f1
Alessandro <alessandro.rotili@gmail.com>
2019-12-20 15:26:53 +0100

    Added BlogPost in models and Admin

--- blogsite/blog/admin.py
+++ blogsite/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+class BlogPostAdmin(admin.ModelAdmin):
+    """Administration object for BlogPost models.
+    Defines:
+     - fields to be displayed in list view (list_display)"""
+    list_display = ("title", "author", "post_date")
+
+admin.site.register(BlogPost, BlogPostAdmin)
--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BlogPost(models.Model):
+    """Model representing the blog post"""
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    content = models.TextField(max_length=5000, help_text="Enter the content of the blog post")
+    post_date = models.DateField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular blog post"""
+        return reverse('blogpost-detail', kwargs={'pk': self.pk}) #args=[str(self.id)]
+
+    def __str__(self):
+        return self.title
